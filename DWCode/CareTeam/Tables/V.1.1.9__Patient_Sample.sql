b0738573462c007520c92c33f13e47a2381dbf65
Siva Sai Pallavi <muppa.siva-sai-pallavi@capgemini.com>
2023-04-27 13:19:04 +0530

    1.1.9

--- DWCode/CareTeam/Tables/V.1.1.9__Patient_Sample.sql
+++ DWCode/CareTeam/Tables/V.1.1.9__Patient_Sample.sql
@@ -1,11 +1,11 @@
-CREATE Schema if not exists  DW_STG_POC;
+/*CREATE Schema if not exists  DW_STG_POC;
 
 USE Schema DW_STG_POC;
 
 CREATE TABLE Sample3_Patient_STG
 (Patient_ID NUMBER
 , P_NAME VARCHAR2(10)
-, P_ADDRESS VARCHAR2(300));
+, P_ADDRESS VARCHAR2(300));*/
 
 CREATE Schema if not exists  DW_TGT_POC;
 
