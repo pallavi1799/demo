d251c276472a071f44e4c32957301321c55b3c7f
Rajgithubtrial <120726003+Rajgithubtrial@users.noreply.github.com>
2023-04-28 18:47:59 +0530

    Add files via upload
    Added new file/updated runslatest.sql

---
+++ DWCode/Common_Utility/A__TGT_table_runslatest.sql
@@ -0,0 +1,11 @@
+CREATE Schema if not exists DW_TGT_POC;
+
+USE Schema DW_TGT_POC;
+
+create table if not exists TGT_Deploymnt_lates1 (
+	Deployment VARCHAR(30),
+	Load_Date  TIMESTAMP_NTZ (9) default current_timestamp()
+
+);
+
+INSERT INTO TGT_Deploymnt_lates1 (Deployment) VALUES ('TEST run time 2: ');
