a557de28ea95327452c79e4d6ff4d01abe160f15
venumaheshuni12 <131147093+venumaheshuni12@users.noreply.github.com>
2023-04-28 18:54:38 +0530

    V.1.1.14__Patient_Sample file added
    V.1.1.14__Patient_Sample file added

---
+++ DWCode/Patient/Tables/V.1.1.14__Patient_Sample .sql
@@ -0,0 +1,13 @@
+
+CREATE Schema if not exists  DW_TGT_POC;
+
+USE Schema DW_TGT_POC;
+
+
+
+CREATE TABLE Sample8_Patient_LKP
+(Patient_ID NUMBER
+, P_LANGUAGE VARCHAR2(10)
+, CITY VARCHAR2(20)
+, AREA VARCHAR2
+	);
